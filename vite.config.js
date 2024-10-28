9548e25941a1afc3f3654198e26c572868b632ce
Aman Thakur <98728221+a3mxn@users.noreply.github.com>
2024-10-29 02:05:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://1vite.dev/config/
 export default defineConfig({
